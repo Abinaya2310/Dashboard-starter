06dc3404ca1c41bfc9b1424c2662d76c5b512388
Abinaya S <abi23.subramanian@gmail.com>
2024-08-17 15:39:37 +0530

    added scroll , sort button

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,23 +1,25 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import IconButton from '@mui/material/IconButton';
-import Paper from '@mui/material/Paper';
-import Snackbar from '@mui/material/Snackbar';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import {
+  Alert,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Paper,
+  Snackbar,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import axios from 'axios';
-import { AnimatePresence, LayoutGroup, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import Popup from '../Popup/Popup';
 import './Table.css';
@@ -31,6 +33,7 @@ export default function BasicTable() {
   const [deleteId, setDeleteId] = useState(null);
   const [selectedRowId, setSelectedRowId] = useState(null);
   const [successMessage, setSuccessMessage] = useState('');
+  const [sortCriteria, setSortCriteria] = useState(''); // State for sorting criteria
 
   const newRowRef = useRef(null); // Ref for the newly added row
 
@@ -42,8 +45,7 @@ export default function BasicTable() {
     try {
       const response = await axios.get('http://localhost:3000/api/entries');
       if (response.status === 200) {
-        const sortedData = response.data.sort((a, b) => new Date(b.date) - new Date(a.date));
-        setData(sortedData);
+        setData(response.data);
       } else {
         console.error('Failed to fetch data:', response.statusText);
       }
@@ -142,94 +144,130 @@ export default function BasicTable() {
     setSuccessMessage('');
   };
 
+  // Sort the data based on the selected criteria
+  const handleSortChange = (e) => {
+    const criteria = e.target.value;
+    setSortCriteria(criteria);
+
+    let sortedData = [...data];
+    if (criteria === 'id') {
+      sortedData.sort((a, b) => a.id.localeCompare(b.id));
+    } else if (criteria === 'date') {
+      sortedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+    } else if (criteria === 'time') {
+      sortedData.sort((a, b) => new Date('1970/01/01 ' + a.date.split(', ')[1]) - new Date('1970/01/01 ' + b.date.split(', ')[1]));
+    } else if (criteria === 'category') {
+      sortedData.sort((a, b) => a.category.localeCompare(b.category));
+    } else if (criteria === 'expense') {
+      sortedData.sort((a, b) => (a.expense || 0) - (b.expense || 0));
+    } else if (criteria === 'income') {
+      sortedData.sort((a, b) => (a.income || 0) - (b.income || 0));
+    } else if (criteria === 'amount') {
+      sortedData.sort((a, b) => a.amount - b.amount);
+    }
+
+    setData(sortedData);
+  };
+
   return (
     <div className="TableContainer">
       <div className="Header">
         <h3>Money Tracker</h3>
-        <Button variant="contained" color="primary" onClick={handleOpenPopup}>
-          Add New Entry
-        </Button>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <select value={sortCriteria} onChange={handleSortChange} className="SortBox" style={{ marginRight: '10px' }}>
+            <option value="">Sort By</option>
+            <option value="id">ID</option>
+            <option value="date">Date</option>
+            <option value="time">Time</option>
+            <option value="category">Category</option>
+            <option value="expense">Expense</option>
+            <option value="income">Income</option>
+            <option value="amount">Amount</option>
+          </select>
+          <Button variant="contained" color="primary" onClick={handleOpenPopup} className="AddNewEntryButton">
+            Add New Entry
+          </Button>
+        </div>
       </div>
-      <TableContainer
-        component={Paper}
-        style={{ maxHeight: 400, overflowY: 'auto', boxShadow: '0px 13px 20px 0px #80808029' }}
-      >
-        <LayoutGroup>
-          <AnimatePresence>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell align="left">DATE</TableCell>
-                  <TableCell align="left">TIME</TableCell>
-                  <TableCell align="left">CATEGORY</TableCell>
-                  <TableCell align="left">EXPENSE</TableCell>
-                  <TableCell align="left">INCOME</TableCell>
-                  <TableCell align="left">AMOUNT</TableCell>
-                  <TableCell align="left">ACTIONS</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((row) => (
-                  <motion.tr
-                    key={row.id}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    layoutId={row.id.toString()}
-                    ref={selectedRowId === row.id ? newRowRef : null} // Assign ref to the new row
-                    className={selectedRowId === row.id ? 'selected-row' : ''}
-                    onClick={() => handleRowClick(row.id)}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.id}
-                    </TableCell>
-                    <TableCell align="left">
-                      {row.date ? row.date.split(', ')[0] : 'N/A'}
-                    </TableCell>
-                    <TableCell align="left">
-                      {row.date ? (row.date.split(', ')[1] ? row.date.split(', ')[1] : 'N/A') : 'N/A'}
-                    </TableCell>
-                    <TableCell align="left">{row.category}</TableCell>
-                    <TableCell align="left">
-                      {row.expense ? row.expense : '-'}
-                    </TableCell>
-                    <TableCell align="left">
-                      {row.income ? row.income : '-'}
-                    </TableCell>
-                    <TableCell align="left">{row.amount}</TableCell>
-                    <TableCell align="left">
-                      <IconButton
-                        color="info"
-                        size="small"
-                        style={{ marginRight: 8 }}
-                        onClick={() => handleView(row)}
-                      >
-                        <VisibilityIcon />
-                      </IconButton>
-                      <IconButton
-                        color="primary"
-                        size="small"
-                        style={{ marginRight: 8 }}
-                        onClick={() => handleEdit(row)}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton
-                        color="secondary"
-                        size="small"
-                        onClick={() => handleDeleteClick(row.id)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </TableCell>
-                  </motion.tr>
-                ))}
-              </TableBody>
-            </Table>
-          </AnimatePresence>
-        </LayoutGroup>
-      </TableContainer>
+      <div className="TableHeader">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="left">DATE</TableCell>
+              <TableCell align="left">TIME</TableCell>
+              <TableCell align="left">CATEGORY</TableCell>
+              <TableCell align="left">EXPENSE</TableCell>
+              <TableCell align="left">INCOME</TableCell>
+              <TableCell align="left">AMOUNT</TableCell>
+              <TableCell align="left" className="actions-header">ACTIONS</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </div>
+      <div className="TableBodyContainer">
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              {data.map((row) => (
+                <motion.tr
+                  key={row.id}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  layoutId={row.id.toString()}
+                  ref={selectedRowId === row.id ? newRowRef : null} // Assign ref to the new row
+                  className={selectedRowId === row.id ? 'selected-row' : ''}
+                  onClick={() => handleRowClick(row.id)}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.date ? row.date.split(', ')[0] : 'N/A'}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.date ? (row.date.split(', ')[1] ? row.date.split(', ')[1] : 'N/A') : 'N/A'}
+                  </TableCell>
+                  <TableCell align="left">{row.category}</TableCell>
+                  <TableCell align="left">
+                    {row.expense ? row.expense : '-'}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.income ? row.income : '-'}
+                  </TableCell>
+                  <TableCell align="left">{row.amount}</TableCell>
+                  <TableCell align="left" className="actions-cell">
+                    <IconButton
+                      color="info"
+                      size="small"
+                      style={{ marginRight: 8 }}
+                      onClick={() => handleView(row)}
+                    >
+                      <VisibilityIcon />
+                    </IconButton>
+                    <IconButton
+                      color="primary"
+                      size="small"
+                      style={{ marginRight: 8 }}
+                      onClick={() => handleEdit(row)}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton
+                      color="secondary"
+                      size="small"
+                      onClick={() => handleDeleteClick(row.id)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </motion.tr>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
 
       {showPopup && (
         <Popup
@@ -248,7 +286,8 @@ export default function BasicTable() {
               <strong>ID:</strong> {viewRow.id}
             </p>
             <p>
-              <strong>Date:</strong> {viewRow.date ? viewRow.date.split(', ')[0] : 'N/A'}</p>
+              <strong>Date:</strong> {viewRow.date ? viewRow.date.split(', ')[0] : 'N/A'}
+            </p>
             <p>
               <strong>Time:</strong> {viewRow.date ? (viewRow.date.split(', ')[1] ? viewRow.date.split(', ')[1] : 'N/A') : 'N/A'}
             </p>
@@ -299,4 +338,6 @@ export default function BasicTable() {
     </div>
   );
 }
+
+
 
